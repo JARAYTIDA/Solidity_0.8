252d647d3a665ac994cb3c4e80314afe2fcf0f5c
JARAYTIDA <rajadityakumawat@gmail.com>
2023-02-24 02:41:09 +0530

    function in sol

--- functions/errors.sol
+++ functions/errors.sol
@@ -2,11 +2,31 @@
 pragma solidity ^0.8.7;
 
 contract ErrorsInSol{
-    function err(uint x) external pure returns(string memory){
-        require( x<=10, "x>10");
+    function testRequire(uint x) external pure returns(string memory){
+        require( x<=10, "x > 10");
         //code
         string memory str = "given number is less then or equal to 10";
         return str;
-        //md
+    }
+
+    function testRevert(uint x) external pure returns(string memory){
+        if(x > 10)
+            revert("x>10");
+        //code
+        string memory str = "given number is less then or equal to 10";
+        return str;
+    }
+
+    uint public num = 10;
+    function testAssert() external view {
+        assert(num == 10);
+    }
+
+    function incr() external {
+        num += 1;
+    }
+
+    function dec() external{
+        num -= 1;
     }
 }
