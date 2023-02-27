09a1e21f9859fd67fc90bda9005a8ac4a0f212c3
JARAYTIDA <rajadityakumawat@gmail.com>
2023-02-27 19:57:13 +0530

    fallback and receive

--- functions/fallback_receive.sol
+++ functions/fallback_receive.sol
@@ -2,6 +2,26 @@
 
 pragma solidity ^0.8.7;
 
+/*
+fallback or receive
+
+    Ether is sent to contract
+                |
+        is my data empty ?
+               / \
+             yes no
+             /     \
+receive() exists?  fallback
+         / \
+       yes  no
+       /     \
+ receive()  fallback()  
+
+
+*/
+
+
+
 contract fallre{
     event Log(string name, address addr, uint val, bytes data);
     fallback() external payable{
